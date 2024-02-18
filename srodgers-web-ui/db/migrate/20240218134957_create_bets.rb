5401ebb074f25328dfec0d7d23ebfc9c582deb54
James Sherriff <james.sherriff@widgit.com>
2024-02-18 16:53:17 +0000

    Update prod db details again for docean

--- srodgers-web-ui/db/migrate/20240218134957_create_bets.rb
+++ srodgers-web-ui/db/migrate/20240218134957_create_bets.rb
@@ -1,13 +1,14 @@
 class CreateBets < ActiveRecord::Migration[7.0]
   def change
-    #create_table :bets do |t|
-      #t.integer :id
-      #t.string :event_id
-      #t.string :market_id
-      #t.string :runner_id
-      #t.float :odds
-      #t.string :status
-      #t.string :offer_id
-    #end
+    create_table :bets do |t|
+      t.integer :id
+      t.string :event_id
+      t.string :market_id
+      t.string :runner_id
+      t.float :odds
+      t.float :stake
+      t.string :status
+      t.string :offer_id
+    end
   end
 end
